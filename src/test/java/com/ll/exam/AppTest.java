8508555ccfce024abd29739d4dcc0867648c983a
SangHun <shzero211@naver.com>
2022-07-13 16:51:27 +0900

    파일 입출력 기능 구현

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,7 +1,16 @@
 package com.ll.exam;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+@Test
+    public void 문자열을_파일에_저장()
+{
+    Util.file.mkdir("test_data");
+    Util.file.saveToFile("test_data/1.txt","안녕");
 
-}
+    String body=Util.file.readFromFile("test_data/1.txt");
+    assertEquals("안녕",body);
+}}
