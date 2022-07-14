96606a6bb849c80932ff9d49c30009a71ba213a9
SangHun <shzero211@naver.com>
2022-07-14 23:34:15 +0900

    App_수정기능_삭제기능_리팩토링

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -28,7 +28,7 @@ public void run(){
                 String author=sc.nextLine().trim();
                 WiseSaying wiseSaying=new WiseSaying(++lastIdx,content,author);
                 wiseSayings.add(wiseSaying);
-                System.out.printf("%d번 명언이 등록되었습니다\n",lastIdx);
+                System.out.printf("%d번 명언이 등록되었습니다.\n",lastIdx);
                 break ;
             case "목록":
                 System.out.println("번호 / 작가 / 명언");
@@ -38,60 +38,64 @@ public void run(){
                 }
                 break;
             case "수정":
-                int mid=rq.getIntValue("id",0);
-
-                if(mid==0){
-                    System.out.println("번호를 입력해주세요.");
-                    break ;
-                }
-                WiseSaying mfoundWiseSaying=null;
-
-                for(WiseSaying wiseSaying_:wiseSayings){
-                    if(wiseSaying_.id==mid){
-                        mfoundWiseSaying=wiseSaying_;
-                    }
-                }
-
-                if(mfoundWiseSaying==null){
-                    System.out.printf("%d번 명언은 존재하지 않습니다.\n",mid);
-                    break;
-                }
-                System.out.printf("명언(기존) : %s\n",mfoundWiseSaying.content);
-                System.out.print("명언 : ");
-                String mcontent=sc.nextLine().trim();
-                System.out.printf("작가(기존) : %s\n",mfoundWiseSaying.author);
-                System.out.print("작가 : ");
-                String mauthor=sc.nextLine().trim();
-                mfoundWiseSaying.content=mcontent;
-                mfoundWiseSaying.author=mauthor;
+                modify(rq);
                 break;
             case "삭제":
-                int id=rq.getIntValue("id",0);
-
-                //Rq의 예외처리가 발생시
-                if(id==0) {
-                    System.out.println("번호를 입력해주세요.");
-                    break;
-                }
-
-                WiseSaying foundWiseSaying=null;
-               for(WiseSaying wiseSaying_:wiseSayings){
-                   if(wiseSaying_.id==id){
-                       foundWiseSaying=wiseSaying_;
-                   }
-               }
-
-               //id와 일치하는 wiseSaying_이 없으면 예외처리
-               if(foundWiseSaying==null){
-                   System.out.printf("%d번 명언은 존재하지 않습니다.\n",id);
-                   break;
-               }
-                wiseSayings.remove(foundWiseSaying);
-                System.out.printf("%d번 명언이 삭제되었습니다.\n",id);
+                remove(rq);
                 break;
             case "종료":
                 break  outer;
         }
     }
+}
+public void modify(Rq rq){
+    int id=rq.getIntValue("id",0);
+
+    if(id==0){
+        System.out.println("번호를 입력해주세요.");
+        return;
+    }
+    WiseSaying wiseSaying=findById(id);
+    if(wiseSaying==null){
+        System.out.printf("%d번 명언은 존재하지 않습니다.\n",id);
+        return;
+    }
+    System.out.printf("명언(기존) : %s\n",wiseSaying.content);
+    System.out.print("명언 : ");
+    String content=sc.nextLine().trim();
+    System.out.printf("작가(기존) : %s\n",wiseSaying.author);
+    System.out.print("작가 : ");
+    String author=sc.nextLine().trim();
+    wiseSaying.content=content;
+    wiseSaying.author=author;
+}
+
+    public void remove(Rq rq){
+        int id=rq.getIntValue("id",0);
+
+        //Rq의 예외처리가 발생시
+        if(id==0) {
+            System.out.println("번호를 입력해주세요.");
+            return;
+        }
+
+        WiseSaying wiseSaying=findById(id);
+
+        //id와 일치하는 wiseSaying_이 없으면 예외처리
+        if(wiseSaying==null){
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n",id);
+            return;
+        }
+        wiseSayings.remove(wiseSaying);
+        System.out.printf("%d번 명언이 삭제되었습니다.\n",id);
+    }
+public WiseSaying findById(int id){
+    WiseSaying foundWiseSaying=null;
+    for(WiseSaying wiseSaying_:wiseSayings){
+        if(wiseSaying_.id==id){
+            foundWiseSaying=wiseSaying_;
+        }
+    }
+    return foundWiseSaying;
 }
 }
